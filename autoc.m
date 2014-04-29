791515c6634804e9a56f08e58ddf6d124ee0106e
TottiPuc <dayan3846@gmail.com>
2014-04-28 22:06:19 -0300

    PDS, fft y envolvente spectral

---
+++ autoc.m
@@ -0,0 +1,13 @@
+function rxx = autoc(x,p)
+
+
+x=x(:);
+
+N=length(x);
+
+for k=0:p
+   
+   rxx(k+1)=x(k+1:N)'*x(1:N-k);
+   
+end
+
--- classificador.m
+++ classificador.m
@@ -23,18 +23,43 @@ for i = 1 : 500;
 	Yframe = enframe (ys(i,:),Nx,160);
         dd = size (Xframe,1)
 	for ii = 1: dd
-%		disp('entro' )
+%%%%%%%%%%% fft %%%%%%%%%%%%%%%%%%%%%%%%%%%
 		X = abs(fft(Xframe(ii,:),NFFT));
-		magx =10*log(X(1:NFFT/2));
-        	Y = abs(fft(Yframe(ii,:),NFFT));
-        	magy = 10*log(Y(1:NFFT/2));
-        	l = length(Xframe(1,:))
-        	t = 0:T:(l-1)*T;
+                Y = abs(fft(Yframe(ii,:),NFFT));
+		magx1 = X(1:NFFT/2);
+                magy1 = Y(1:NFFT/2);
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+     %%%%%%%% PDS %%%%%%%%%%%%
+                X1 = fft(Xframe(ii,:),NFFT);
+		PDSx = X1.*conj(X1);
+                magx = PDSx(1:NFFT/2);
+        	Y1 = fft(Yframe(ii,:),NFFT);
+		PDSy = Y1.*conj(Y1);
+        	magy = PDSy(1:NFFT/2);
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+     %%%%%%%% lpc %%%%%%%%%%%%
+
+		p = fs/1000 + 4;
 		f = fs/2*linspace(0,1,NFFT/2);
+		[a,g] = ilpc (magx,p);
+		[b,h] = ilpc (magy,p);
+                [a1,g1] = ilpc (magx1,p);
+                [b1,h1] = ilpc (magy1,p);
+		lspecX = freqz(g,a,f,fs);
+                lspecY = freqz(h,b,f,fs);
+                lspecX1 = freqz(g1,a1,f,fs);
+                lspecY1 = freqz(h1,b1,f,fs);
+        	l = length(Xframe(1,:));
+        	t = 0:T:(l-1)*T;
 		subplot(4,1,1), plot(t,Xframe(1,:))
 		subplot(4,1,2), plot(f,10*log(magx/NFFT)) 
                 subplot(4,1,3), plot(t,Yframe(1,:))
                 subplot(4,1,4), plot(f,10*log(magy/NFFT))
+		figure
+                subplot(4,1,1), plot(f,20*log10(abs(lspecX1)),'k')
+		subplot(4,1,2), plot(f,20*log10(abs(lspecX)),'k')
+                subplot(4,1,3), plot(f,20*log10(abs(lspecY1)),'k')
+		subplot(4,1,4), plot(f,20*log10(abs(lspecY)),'k')
 		pause
 	end
 
